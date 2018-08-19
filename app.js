58b1f2d9efe41a1b645aacb9b4300bcbde6ae148
alexgev <gevorkyan.alexey@gmail.com>
2018-08-19 17:02:55 +0300

    feat: redirect from heroku

---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require('http'),
+  httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+console.log('listening on port 8000');
+httpProxy.createProxyServer({target:'http://142.93.141.134/'}).listen(8000);
