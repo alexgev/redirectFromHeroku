6408d3feec39f1ae45262f891bfdd9c376d8e1d6
alexgev <gevorkyan.alexey@gmail.com>
2018-08-19 19:12:19 +0300

    change port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var http = require('http'),
 // Create your proxy server and set the target in the options.
 //
 console.log('listening on port 8000');
-httpProxy.createProxyServer({target:'http://142.93.141.134:1337/'}).listen(1337);
+httpProxy.createProxyServer({target:'http://142.93.141.134:1337/'}).listen(process.env.PORT || 1337);
